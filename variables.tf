28a8178153c40afcbe8333584e5e2c209842babc
OFFISONG EMMANUEL <OFFISONG@OFFISONGS-MacBook-Pro.local>
2023-03-11 00:14:24 +0100

    Adding variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,31 @@
 variable "region" {
     type = string
 }
+
+
+#Code build iam
+variable "iam_role" {
+    type = string
+}
+variable "iam_resources" {}
+
+
+# Code build project
+variable "project_name" {
+    type = string
+}
+variable "project_description" {
+    type = string
+}
+variable "project_timeout" {
+    type = string
+}
+variable "github_name" {
+    type = string
+}
+
+#Code deploy deploy
+variable "app_name"{}
+variable "compute_platform" {
+    type = string
+}
