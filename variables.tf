674b368bdb9b3ec14babecc6913fb1d14a140845
OFFISONG EMMANUEL <OFFISONG@OFFISONGS-MacBook-Pro.local>
2023-03-11 08:51:26 +0100

    Adding variables.tf

--- variables.tf
+++ variables.tf
@@ -26,8 +26,24 @@ variable "github_name" {
     type = string
 }
 
+variable "github_branch" {
+    type = string
+}
+variable "github_repo" {
+    type = string
+}
+
 #Code deploy deploy
 variable "app_name"{}
 variable "compute_platform" {
     type = string
 }
+
+#ECR
+variable "ecr_repo_name" {}
+
+#EC2
+variable "owner_id" {}
+variable "instance_type" {}
+variable "policy_arn" {}
+variable "deployment_group_name" {}
